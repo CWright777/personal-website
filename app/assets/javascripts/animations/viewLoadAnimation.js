55fc7d6a0f264adfb05aab15e9272b0009cbaae0
Cliff Wright <clifford@cliffordwright.com>
2016-05-14 16:55:25 -0700

    animation progress

--- app/assets/javascripts/animations/viewLoadAnimation.js
+++ app/assets/javascripts/animations/viewLoadAnimation.js
@@ -1,54 +1,66 @@
-angular.module('cliffWebsite')
-.animation('.animated-view', function() {
-return {
-    enter : function(element, done) {
-      element.css('opacity',1);
-      jQuery(element).animate({
-        opacity: 0
-      }, done);
+//angular.module('cliffWebsite')
+//.animation('.animated-view', function() {
+//return {
+    //addClass: function(element, className){
+      //TweenMax.to(element, 1, {
+        //'opacity': 0
+      //});
+    //},
+    //removeClass: function(element, className){
+      //TweenMax.to(element, 1, {
+        //'opacity': 1
+      //});
+    //}
+  //}
+//}); 
+    ////enter : function(element, done) {
+      ////element.css('opacity',1);
+      ////jQuery(element).animate({
+        ////opacity: 0
+      ////}, done);
 
-      // optional onDone or onCancel callback
-      // function to handle any post-animation
-      // cleanup operations
-      return function(isCancelled) {
-        if(isCancelled) {
-          jQuery(element).stop();
-        }
-      }
-    },
-    leave : function(element, done) {
-      element.css('opacity', 1);
-      jQuery(element).animate({
-        opacity: 0
-      }, done);
+      ////// optional onDone or onCancel callback
+      ////// function to handle any post-animation
+      ////// cleanup operations
+      ////return function(isCancelled) {
+        ////if(isCancelled) {
+          ////jQuery(element).stop();
+        ////}
+      ////}
+    ////},
+    ////leave : function(element, done) {
+      ////element.css('opacity', 1);
+      ////jQuery(element).animate({
+        ////opacity: 0
+      ////}, done);
 
-      // optional onDone or onCancel callback
-      // function to handle any post-animation
-      // cleanup operations
-      return function(isCancelled) {
-        if(isCancelled) {
-          jQuery(element).stop();
-        }
-      }
-    },
-    move : function(element, done) {
-      element.css('opacity', 0);
-      jQuery(element).animate({
-        opacity: 1
-      }, done);
+      ////// optional onDone or onCancel callback
+      ////// function to handle any post-animation
+      ////// cleanup operations
+      ////return function(isCancelled) {
+        ////if(isCancelled) {
+          ////jQuery(element).stop();
+        ////}
+      ////}
+    ////},
+    ////move : function(element, done) {
+      ////element.css('opacity', 0);
+      ////jQuery(element).animate({
+        ////opacity: 1
+      ////}, done);
 
-      // optional onDone or onCancel callback
-      // function to handle any post-animation
-      // cleanup operations
-      return function(isCancelled) {
-        if(isCancelled) {
-          jQuery(element).stop();
-        }
-      }
-    },
+      ////// optional onDone or onCancel callback
+      ////// function to handle any post-animation
+      ////// cleanup operations
+      ////return function(isCancelled) {
+        ////if(isCancelled) {
+          ////jQuery(element).stop();
+        ////}
+      ////}
+    ////},
 
-    // you can also capture these animation events
-    addClass : function(element, className, done) {},
-    removeClass : function(element, className, done) {}
-  }
-});
+    ////// you can also capture these animation events
+    ////addClass : function(element, className, done) {},
+    ////removeClass : function(element, className, done) {}
+  ////}
+////});
--- app/assets/javascripts/directives/viewLoadDirective.js
+++ app/assets/javascripts/directives/viewLoadDirective.js
@@ -1,24 +1,30 @@
 angular.module('cliffWebsite')
 .directive('viewLoad', function ($compile,$animate,viewService,$timeout) {
   return {
-    restrict:'E',
-    link: function(scope,el,attrs){
-      el.html("<ui-view name='main' class='slider full-width'></div>")
-      $compile(el.contents())(scope)
+    //restrict:'E',
+    link: function(scope,elem,attrs){
+      elem.html("<ui-view name='main' class='slider full-width'></div>")
+      $compile(elem.contents())(scope)
       scope.$on('view_changed',function(){
         var view = viewService.getViewName()
-        angular.element(el[0]).append("<ui-view name='"+view+"' class='animated-view slider full-width'></div>")
-        $compile(el.contents())(scope)
-        console.log(angular.element(el.children()))
+        elem.append("<ui-view name='"+view+"' class='slider full-width'></div>")
+        $compile(elem.contents())(scope)
+        $animate.addClass(elem,'hide-add-start')
+        $timeout(function(){
+          $animate.removeClass(elem,'hide')
+        },3000)
+        })
+
         //$animate.leave(angular.element(el.contents(),el))
-        $animate.enter(angular.element(el.children()[1]),el,el.parent())
+
+        //$animate.enter(angular.element(el.children()[1]),el,el.parent())
 
         //angular.element(el[0].children[0]).addClass('closed')
         //angular.element(el[0].children[1]).addClass('open')
-      })
+      }
     }
   }
-})
+)
         //var view = viewService.getViewName()
         //console.log(angular.element(el[0].children[0]))
         //angular.element(el[0].contents).append($compile("<ui-view name='"+view+"' class='slider full-width'></div>")(scope))
@@ -27,3 +33,30 @@ angular.module('cliffWebsite')
         //$animate.enter(newView,el,el.parent()[0])
         //angular.element(el[0].children[0]).addClass('closed')
         //angular.element(el[0].children[1]).addClass('open')
+//
+//
+//
+//angular.module('cliffWebsite')
+//.directive('viewLoad', function ($compile,$animate,viewService,$timeout) {
+  //return {
+    ////restrict:'E',
+    //link: function(scope,el,attrs){
+      //el.html("<ui-view name='main' class='slider full-width'></div>")
+      //$compile(el.contents())(scope)
+      //scope.$on('view_changed',function(){
+        //var view = viewService.getViewName()
+        //el.append("<ui-view name='"+view+"' class='animated-view slider full-width'></div>")
+        //$compile(el.contents())(scope)
+
+        //$animate.addClass(el.children()[0],'hide-js')
+
+        ////$animate.leave(angular.element(el.contents(),el))
+
+        ////$animate.enter(angular.element(el.children()[1]),el,el.parent())
+
+        ////angular.element(el[0].children[0]).addClass('closed')
+        ////angular.element(el[0].children[1]).addClass('open')
+      //})
+    //}
+  //}
+//})
