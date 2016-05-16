3b00a41daef92084e03b69e0b8d30b9ee75bea8a
Cliff Wright <clifford@cliffordwright.com>
2016-05-15 23:37:03 -0700

    add technology / animation styling

---
+++ app/assets/javascripts/directives/hoverImgDirective.js
@@ -0,0 +1,20 @@
+angular.module('cliffWebsite')
+.directive('imgHover', function ($animate) {
+  return {
+    link: function(scope,elem,attrs) {
+      console.log()
+      elem.bind('mouseenter', function(){
+        elem[0].querySelector('img').style.transform = 'translate3d(-5vh,-5vh,0)'
+        elem[0].querySelector('.tile-footer').style.height = '65%'
+        elem[0].querySelector('h1').style.fontSize = '1.3em'
+        elem[0].querySelector('img').style.opacity = '0'
+      });
+      elem.bind('mouseleave',function(){
+        elem[0].querySelector('img').style.transform = 'translate3d(0,0,0)'
+        elem[0].querySelector('.tile-footer').style.height = '30% '
+        elem[0].querySelector('h1').style.fontSize = '1.1em'
+        elem[0].querySelector('img').style.opacity = '1'
+      })
+    }
+  }
+})
